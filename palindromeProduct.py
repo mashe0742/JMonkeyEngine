56bef7e2d95148ed93597d09e5bc161552a31d27
Michael Ashe <mashe0742@gmail.com>
2019-06-05 20:23:25 -0400

    Verifying change to directory path

---
+++ palindromeProduct.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Script to find largest palindrome product of two n-length
+integers for Project Euler.
+
+Made by Michael Ashe.
+"""
+
+#define variables
+z = x+y
+
+#take user input for length of n
+try:
+    n = int(input('Enter n length for operators in equation: '))
+except ValueError:
+    print('n must be an integer!')
+
+    
