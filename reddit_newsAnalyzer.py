fd5adcf72be3e92c5dc0edbe057ba03d3b8a16b7
Michael Ashe <mashe0742@gmail.com>
2019-06-06 21:57:22 -0400

    Implemented PRAW access to Reddit API for /r/politics

--- reddit_newsAnalyzer.py
+++ reddit_newsAnalyzer.py
@@ -28,6 +28,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 from nltk.tokenize import word_tokenize, sent_tokenize, RegexpTokenizer
 from nltk.corpus import stopwords
 stop_words = stopwords.words("english")
+import praw
 
 #function for tokenization and cleaning of headlines
 def process_text(headlines):
@@ -40,37 +41,17 @@ def process_text(headlines):
     
     return tokens
 
-#variable for url of page and dictionary to store headlines
-target_page = 'https://www.reuters.com/'
-headlinesDict = {}
+#connect to reddit API using PRAW
+reddit = praw.Reddit(client_id='CLIENT ID',
+                     client_secret='CLIENT SECRET',
+                     user_agent='USERNAME')
 
-#headers to pass useragent in order to evade bot detection
-req = urllib2.Request(target_page, None, {'User-agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'})
-response = urllib2.urlopen(req).read()
+#actual connection to defined subreddit, retrieve 1000 new posts
+headlines = set()
 
-#query target page and return html var
-#then use beautifulsoup parser to read page
-page = response
-soup = BeautifulSoup(page, 'html.parser')
-
-print(soup)
-#parse html response looking for Reuters article tag, then save off all headlines
-tags = soup.find_all('article')
-
-print(tags)
-
-row = 0
-for tag in tags:
-    #drop time stamps, unwanted header lines
-    if not any(value in tag.text for value in ("pm EDT", "h ago", "m ago", "Editor's Choice", "SUSTAINABLE BUSINESS", "SECTORS UP CLOSE")):
-        headline = re.sub('(\n){2,}', ' ', tag.text.strip())
-        if not any (value in headline for value in ("\n")):
-            headlinesDict[row]=headline
-            row+=1
-
-
-#sanitation to remove artifacts of scrape
-headlinesDictCleaned = {k: v for k, v in headlinesDict.items() if v is not None}
+for submission in reddit.subreddit('politics').new(limit=None):
+    headlines.add(submission.title)
+    print(len(headlines))
 
 #set up sentiment analyzer
 sia = SIA()
