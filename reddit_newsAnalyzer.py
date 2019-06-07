1a7c0f77a81a5d070d720b8c1e1600e6bad37d96
Michael Ashe <mashe0742@gmail.com>
2019-06-06 22:28:05 -0400

    Finished coding log-log plot for negative word frequency

--- reddit_newsAnalyzer.py
+++ reddit_newsAnalyzer.py
@@ -37,8 +37,8 @@ def process_text(headlines):
 
 #connect to reddit API using PRAW
 reddit = praw.Reddit(client_id='CLIENT ID',
-                     client_secret='CLIENT SECRET',
-                     user_agent='USER ID')
+                     client_secret='CLIENT PRIVATE',
+                     user_agent='USERNAME')
 
 #actual connection to defined subreddit, retrieve 1000 new posts
 headlines = set()
@@ -67,7 +67,7 @@ df.loc[df['compound'] < -0.2, 'label'] = -1
 #save to csv file
 df2 = df[['headline', 'label']]
 #df2.to_csv('reuters_headline_labels.csv', mode='a',encoding='utf-8',index=False)
-print(df2.label.value_counts)
+
 print(df2.label.value_counts(normalize=True)*100)
 
 #plot bar graph of results from sentiment analysis - Pos, Neut, Neg
@@ -112,5 +112,41 @@ fig = plt.figure(figsize=(10,5))
 plt.xlabel("Words (Log)")
 plt.ylabel("Frequency (Log)")
 plt.title("Word Frequency Distribution, Log (Positive)")
+plt.plot(x_val, y_final)
+plt.show()
+
+#Analyze negative headlines, word distributions
+neg_lines = list(df2[df2.label == -1].headline)
+
+neg_tokens = process_text(neg_lines)
+neg_freq = nltk.FreqDist(neg_tokens)
+
+print(neg_freq.most_common(20))
+
+#generate plots for distribution of negative words
+y_val = [x[1] for x in neg_freq.most_common()]
+
+fig = plt.figure(figsize=(10,5))
+plt.plot(y_val)
+
+plt.xlabel("words")
+plt.ylabel("Frequency")
+plt.title("Word Frequency Distribution (Negative)")
+plt.show()
+
+#generate log-log plot of negative word distribution
+y_final = []
+for i,k,z in zip(y_val[0::3], y_val[1::3], y_val[2::3]):
+    if i+k+z ==0:
+        break
+    y_final.append(math.log(i+k+z))
+    
+x_val = [math.log(i+1) for i in range(len(y_final))]
+
+fig = plt.figure(figsize=(10,5))
+
+plt.xlabel("Words (Log)")
+plt.ylabel("Frequency (Log)")
+plt.title("Word Frequency Distribution, Log (Negative)")
 plt.plot(x_val, y_final)
 plt.show()
